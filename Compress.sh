ff90ba668d4d44eb60be50748d2bcb39cc4d2c25
lll69 <60803753+lll69@users.noreply.github.com>
2022-05-16 13:44:43 +0800

    Update Compress.sh

--- Compress.sh
+++ Compress.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo npm install uglify-js -g
 find build/js -name '*.js' | while read f; do uglifyjs $f -c -m --no-annotations -o $f;done
+echo '<meta http-equiv="refresh" content="0;url=https://turbowarp.org/embed.html">' > build/embed.html
 exit 0
