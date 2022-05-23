1b467311daf5ea9fe80004c850e97e497e4c41ed
lll69 <60803753+lll69@users.noreply.github.com>
2022-05-23 14:49:21 +0800

    Update Compress.sh

--- Compress.sh
+++ Compress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo npm install uglify-js -g
-find build/ -name '*.html' | while read f; do echo -e '\ns=document.createElement("link");s.rel="icon";s.href="https://lll69.github.io/turbo-warp-scratch-gui/favicon.ico";document.head.appendChild(s);' >> $f;done
+find build/ -name '*.html' | while read f; do echo -e '\n<script>s=document.createElement("link");s.rel="icon";s.href="https://lll69.github.io/turbo-warp-scratch-gui/favicon.ico";document.head.appendChild(s);</script>' >> $f;done
 find build/js -name '*.js' | while read f; do uglifyjs $f -c --no-annotations -o $f;done
 echo '<meta http-equiv="refresh" content="0;url=https://turbowarp.org/embed.html">' > build/embed.html
 exit 0
