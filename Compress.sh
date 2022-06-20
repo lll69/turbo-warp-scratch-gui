a87f8b6ec4b48375f6063c23fa8f718fdf300119
lll69 <60803753+lll69@users.noreply.github.com>
2022-06-20 10:54:40 +0800

    Update

--- Compress.sh
+++ Compress.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+touch build/.nojekyll
 sudo npm install uglify-js -g
 find build/ -name '*.html' | while read f; do echo -e '\n<script>s=document.createElement("link");s.rel="icon";s.href="https://lll69.github.io/turbo-warp-scratch-gui/favicon.ico";document.head.appendChild(s);</script>' >> $f;done
 find build/ -name '*.js' | while read f; do uglifyjs $f -c --no-annotations -o $f;done
