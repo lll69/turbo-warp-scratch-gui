713eb4f2b9a1fe0856dcb2668c42d1fa62c015d8
lll69 <60803753+lll69@users.noreply.github.com>
2022-06-20 16:20:51 +0800

    Update 404

--- Compress.sh
+++ Compress.sh
@@ -3,6 +3,8 @@ touch build/.nojekyll
 sudo npm install uglify-js -g
 find build/ -name '*.html' | while read f; do echo -e '\n<script>s=document.createElement("link");s.rel="icon";s.href="https://lll69.github.io/turbo-warp-scratch-gui/favicon.ico";document.head.appendChild(s);</script>' >> $f;done
 find build/ -name '*.js' | while read f; do uglifyjs $f -c --no-annotations -o $f;done
-echo '<meta http-equiv="refresh" content="0;url=https://turbowarp.org/embed.html">' > build/embed.html
-echo '<meta http-equiv="refresh" content="0;url=https://experiments.turbowarp.org/pointerlock/embed.html">' > build/pointerlock/embed.html
+#echo '<meta http-equiv="refresh" content="0;url=https://turbowarp.org/embed.html">' > build/embed.html
+mv build/embed.html build/embed.html.txt
+#echo '<meta http-equiv="refresh" content="0;url=https://experiments.turbowarp.org/pointerlock/embed.html">' > build/pointerlock/embed.html
+mv build/pointerlock/embed.html build/pointerlock/embed.html.txt
 exit 0
